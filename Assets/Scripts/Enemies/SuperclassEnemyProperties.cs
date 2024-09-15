2c4976e185aca5f8b4a2d6a0a7e67cae0c76dfb4
Justin Torres <justintorres631@gmail.com>
2024-09-15 13:42:27 -0400

    Defeated enemies launch as additional projectiles.
    Defeated enemies are now launched with billiards physics and can damage undefeated enemies.

--- Assets/Scripts/Enemies/SuperclassEnemyProperties.cs
+++ Assets/Scripts/Enemies/SuperclassEnemyProperties.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class SuperclassEnemyProperties : MonoBehaviour
 {
     // This script is intended to be a superclass for enemies. Any properties that all enemies will share should be here.
     protected Rigidbody2D enemyRb;
+    protected CircleCollider2D enemyCollider;
     protected PlayerControls playerScript;
     // Stats all enemies should have.
     [SerializeField] protected float health = 1.0f;
     [SerializeField] protected float speed = 2.0f;
+    [SerializeField] protected float launchSpeed = 20.0f;
+    [SerializeField] protected float launchTime = 4.0f;
     // Invincibility frame to prevent the same bullet from damaging twice.
     private bool vulnerable = true;
     [SerializeField] private float damageCooldown = 0.1f;
+    // Physics reference. Finds direction projectile hits from.
+    Vector3 projectileDirection;
     // Start is called before the first frame update
     void Start()
     {
-        // Get the enemy's rigidbody and the player's script to be able to deal damage.
+        // Get the enemy's rigidbody, collider and the player's script to be able to deal damage.
         enemyRb = GetComponent<Rigidbody2D>();
+        enemyCollider = GetComponent<CircleCollider2D>();
         playerScript = GameObject.Find("Player").GetComponent<PlayerControls>();
     }
 
@@ -40,8 +47,10 @@ public class SuperclassEnemyProperties : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         //UnityEngine.Debug.Log("Trigger Detected");
-        if (other.gameObject.CompareTag("PlayerAttack") && vulnerable)
+        if (other.gameObject.CompareTag("PlayerAttack") && vulnerable && health >= 1.0f)
         {
+            // Remember direction projectile hit from.
+            projectileDirection = -(other.transform.position - transform.position).normalized;
             // Destroy the player's bullet that hit.
             Destroy(other.gameObject);
             // Damage enemy.
@@ -59,7 +68,8 @@ public class SuperclassEnemyProperties : MonoBehaviour
         //UnityEngine.Debug.Log("Damaged Enemy");
         if (health <= 0)
         {
-            EnemyDeath();
+            Launch(projectileDirection);
+            //EnemyDeath();
         }
        
     }
@@ -70,9 +80,23 @@ public class SuperclassEnemyProperties : MonoBehaviour
         yield return new WaitForSeconds(damageCooldown);
         vulnerable = true;
     }
+    // Launch enemy and set a timer before destroying the Game Object.
+    public void Launch(Vector3 direction) {
+        // Unfreeze Constraints.
+        enemyRb.constraints = RigidbodyConstraints2D.None;
+        // Set tag to PlayerAttack.
+        gameObject.tag = "PlayerAttack";
+        // Set IsTrigger to true.
+        enemyCollider.isTrigger = true;
+        // Apply force.
+        enemyRb.AddForce(direction * launchSpeed, ForceMode2D.Impulse);
+        // Trigger countdown for enemy death.
+        StartCoroutine(EnemyDeath());
+    }
     // Destroy the enemy, and trigger anything you want to happen upon death.
-    public void EnemyDeath()
+    public IEnumerator EnemyDeath()
     {
+        yield return new WaitForSeconds(launchTime);
         Destroy(gameObject);
     }
 }
